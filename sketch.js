7db5e875c445a0f53dcf42b46bda982471c7a4fc
Kumari Anamika <75468908+IAFO-KumariAnamika@users.noreply.github.com>
2021-09-18 16:44:16 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,54 @@
+var words=["Never","Look","Back"];
+var input,button,element;
+var i=0;
+var ival;
+var value;
+var gameState=0;
+
+function setup(){
+    createCanvas(800,600);
+
+    element=createElement('h2');
+    element.position(200,290);
+    
+    input=createInput("",'text').attribute("placeholder","Write the above text");
+    input.position(300,390);
+
+    button=createButton("SUBMIT");
+    button.position(400,490);
+}
+
+function draw(){
+background(255);
+
+textSize(40);
+text("Press N for next word",500,200);
+element.html(words[i]);
+
+if(keyWentDown('N') && gameState===1 && i<words.length){
+    gameState=0;
+  
+    i++;
+}
+
+ival=input.value();
+button.mousePressed(()=>{
+    gameState=1
+    if(ival===words[i]){
+        value=1;
+    }
+    else{
+        value=2;
+    }
+    if(value===1){
+        text("Correct",400,100);
+    }
+    if(value===2){
+        text("wrong",100,100);
+    }
+    
+})
+
+drawSprites();
+
+}
